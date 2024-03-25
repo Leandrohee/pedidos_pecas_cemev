f187f05360d8ecd6855dc66b3bbb08395611b8f8
Leandro Torres <123303114+Leandrohee@users.noreply.github.com>
2024-03-25 19:58:19 -0300

    Update admin.py

--- pedidos_oficina/admin.py
+++ pedidos_oficina/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Pedido
+
+class PedidoAdmin(admin.ModelAdmin):
+    
+    list_display = ('id','oficina','oss','prefixo','modelo','fornecedor', 'data_envio','data_recebimento','responsavel_recebimento','mecanico_requerente','nota_fiscal')
+    list_display_links = ('id',)
+    search_fields = ('id','prefixo')
+    list_per_page = 100
+
+
+admin.site.register(Pedido,PedidoAdmin)
+
