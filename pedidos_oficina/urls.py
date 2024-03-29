a21adffd0161cc88b62130fe3541359f79899009
leandroTorres <leandrohenrique_@live.com>
2024-03-29 19:30:49 -0300

    arrumei umas coisas

--- pedidos_oficina/urls.py
+++ pedidos_oficina/urls.py
@@ -6,10 +6,10 @@ from .serializer_views import Pedido2024ViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()                                                                #criando uma rota base para acessar a api 
-router.register(r'pedidos2024', Pedido2024ViewSet, basename='pedidos2024')                      #criando a rota relativa 'pedidos2024/' para acessar o modelo Pedido2024 com o serializer
+router.register('pedidos2024', Pedido2024ViewSet, basename='pedidos2024')                      #criando a rota relativa 'pedidos2024/' para acessar o modelo Pedido2024 com o serializer
 
 
 urlpatterns = [
-    path('',include(router.urls)),                                                              #essa rota padrao direciona para as rotas relativas criadas usando o viewset
+    path('api_',include(router.urls)),                                                              #essa rota padrao direciona para as rotas relativas criadas usando o viewset
     path('',renderizandoReact, name='pedidosOficina'),
 ]
